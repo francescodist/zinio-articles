fcf732738a44392b6e06c40c681f1df649248928
francescodist <francesco.dstf@gmail.com>
2019-09-28 14:09:35 +0100

    added styling for App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {Redirect, Route, Switch} from "react-router-dom";
 import MagazinesView from "./components/magazines-view";
+import styled from "styled-components";
 
 function App() {
   return (
-      <Switch>
-          <Route exact path="/magazines"  component={MagazinesView} />
-          <Redirect to="/magazines" />
-      </Switch>
+      <AppView>
+          <Switch>
+              <Route exact path="/magazines"  component={MagazinesView} />
+              <Redirect to="/magazines" />
+          </Switch>
+      </AppView>
   );
 }
+
+const AppView = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: aliceblue;
+`;
 
 export default App;
