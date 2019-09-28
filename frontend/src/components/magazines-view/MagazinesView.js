08c3ab05a5bc5abd53300722127f351decd86af2
francescodist <francesco.dstf@gmail.com>
2019-09-28 14:24:49 +0100

    added header to magazines view

--- frontend/src/components/magazines-view/MagazinesView.js
+++ frontend/src/components/magazines-view/MagazinesView.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import styled from "styled-components";
 
 function MagazinesView() {
     return (
-        <div/>
+        <MagazinesViewContainer>
+            <MagazinesViewHeader>
+                <MagazineTitle>Title</MagazineTitle>
+            </MagazinesViewHeader>
+        </MagazinesViewContainer>
     );
 }
+
+const MagazinesViewContainer = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+const MagazinesViewHeader = styled.header`
+  height: 64px;
+  width: 100%;
+  background-color: #1091bd;
+  display: flex;
+  font-size: 30px;
+  color: #ffffff;
+`;
+
+const MagazineTitle = styled.div`
+  margin: auto;
+`;
 
 export default MagazinesView;
