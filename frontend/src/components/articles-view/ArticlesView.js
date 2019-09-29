3576da16fb04327928c54f3f9c9f09798ad20e9f
francescodist <francesco.dstf@gmail.com>
2019-09-29 15:40:07 +0100

    added search for articles

--- frontend/src/components/articles-view/ArticlesView.js
+++ frontend/src/components/articles-view/ArticlesView.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {
+    ArticleSearchButton, ArticleSearchInput, ArticleSearchResult, ArticleSearchResults,
     ArticlesViewBody,
     ArticlesViewContainer,
     ArticlesViewFooter,
@@ -12,6 +13,7 @@ function ArticlesView() {
 
     const [articles, setArticles] = useState(null);
     const [selectedIndex, setSelectedIndex] = useState(null);
+    const [search, setSearch] = useState(null);
 
     useEffect(() => {
         fetchArticles().then(articles => {
@@ -28,10 +30,41 @@ function ArticlesView() {
         setSelectedIndex(selectedIndex + 1)
     }
 
+    function toggleSearch() {
+        if (search !== null) {
+            setSearch(null);
+        } else {
+            setSearch('');
+        }
+    }
+
+    function searchInput(event) {
+        setSearch(event.target.value)
+    }
+
+    function selectSearchResult() {
+        setSearch(null);
+    }
+
     return (
         <ArticlesViewContainer>
             <ArticlesViewHeader>
-                <ArticleTitle>{articles ? articles[selectedIndex].title : 'Articles'}</ArticleTitle>
+                {search === null ?
+                    <ArticleTitle>{articles ? articles[selectedIndex].title : 'Articles'}</ArticleTitle>
+                    : <ArticleSearchInput onChange={searchInput} value={search}/>}
+                <ArticleSearchButton isSearching={search !== null} onClick={toggleSearch}/>
+                {search && search.length > 0 ?
+                    <ArticleSearchResults>
+                        <ArticleSearchResult onClick={selectSearchResult}>
+                            Result 1
+                        </ArticleSearchResult>
+                        <ArticleSearchResult onClick={selectSearchResult}>
+                            Result 2
+                        </ArticleSearchResult>
+                        <ArticleSearchResult onClick={selectSearchResult}>
+                            Result 3
+                        </ArticleSearchResult>
+                    </ArticleSearchResults> : ''}
             </ArticlesViewHeader>
             <ArticlesViewBody dangerouslySetInnerHTML={{__html: articles ? articles[selectedIndex].body : ''}}/>
             <ArticlesViewFooter>
--- frontend/src/components/articles-view/ArticlesViewStyles.js
+++ frontend/src/components/articles-view/ArticlesViewStyles.js
@@ -8,6 +8,25 @@ const defaultBar = css`
   display: flex;
 `;
 
+const defaultButton = css`
+  background: none;
+  color: white;
+  font-size: 24px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: background-color 0.2s linear 0s;
+  
+  &:hover {
+    background-color: rgba(255,255,255,0.2);
+  }
+  
+  &:active {
+    background-color: rgba(195,195,195,0.2);
+    transition: none;
+  }
+`;
+
 export const ArticlesViewContainer = styled.div`
   height: 100%;
   width: 100%;
@@ -19,8 +38,9 @@ export const ArticlesViewHeader = styled.header`
   ${defaultBar};
   font-size: 30px;
   color: #ffffff;
+  position: relative;
   
-  @media(max-width: 400px) {
+  @media(max-width: 600px) {
     & {
       font-size: 18px;
     }
@@ -29,11 +49,72 @@ export const ArticlesViewHeader = styled.header`
 
 export const ArticleTitle = styled.div`
   margin: auto;
-  padding: 0 10px;
+  padding: 0 64px 0 10px;
   max-width: 100%;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+`;
+
+export const ArticleSearchInput = styled.input`
+  margin: auto 64px;
+  padding: 0 10px;
+  font-size: 30px;
+  color: white;
+  height: 50px;
+  background-color: transparent;
+  border: none;
+  border-bottom: 2px solid white;
+  outline: none;
+  max-width: 100%;
+  width: 100%;
+  
+  @media(max-width: 600px) {
+    & {
+      margin-left: 10px;
+    }
+  }
+`;
+
+
+export const ArticleSearchButton = styled.button`
+  ${defaultButton};
+  height: 40px;
+  width: 40px;
+  position: absolute;
+  right: 15px;
+  top: 12px;
+  border-radius: 50%;
+  background-image: ${props => props.isSearching ? 'url("/icons/close.svg")' : 'url("/icons/search.svg")'}; ;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 20px;
+`;
+
+export const ArticleSearchResults = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 64px;
+  background-color: white;
+`;
+
+export const ArticleSearchResult = styled.div`
+  max-width: 100vw;
+  font-size: 20px;
+  padding: 10px 10px;
+  text-align: center;
+  color: black;
+  border-bottom: 1px solid #ddd;
+  cursor: pointer;
+  transition: background-color 0.2s linear 0s;
+  
+  &:hover {
+    background-color: #f0f8ff;
+  }
+  
+  &:active {
+    background-color: #e2eaf1;
+  }
 `;
 
 export const ArticlesViewBody = styled.div`
@@ -64,22 +145,7 @@ export const FooterNavigationButtons = styled.div`
 
 export const NavigationButton = styled.button`
   margin: auto 15px auto 0;
-  background: none;
-  color: white;
-  font-size: 24px;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  transition: background-color 0.2s linear 0s;
+  ${defaultButton};
   padding: 5px 10px;
   border-radius: 5px;
-  
-  &:hover {
-    background-color: rgba(255,255,255,0.2);
-  }
-  
-  &:active {
-    background-color: rgba(195,195,195,0.2);
-    transition: none;
-  }
 `;
