38a4ef9561b65c38854f3b65136489c7db3443e7
francescodist <francesco.dstf@gmail.com>
2019-09-29 12:56:31 +0100

    added footer functionalities to articles view

--- frontend/src/components/articles-view/ArticlesView.js
+++ frontend/src/components/articles-view/ArticlesView.js
@@ -4,26 +4,49 @@ import {
     ArticlesViewContainer,
     ArticlesViewFooter,
     ArticlesViewHeader,
-    ArticleTitle
+    ArticleTitle, FooterContainer, FooterNavigationButtons, FooterPageIndex, NavigationButton
 } from "./ArticlesViewStyles";
-import {fetchArticles} from "../../actions/ArticlesActions";
+import {fetchArticles} from "../../actions";
 
 function ArticlesView() {
 
     const [articles, setArticles] = useState(null);
+    const [selectedIndex, setSelectedIndex] = useState(null);
 
     useEffect(() => {
-        fetchArticles().then(articles => setArticles(articles));
+        fetchArticles().then(articles => {
+            setSelectedIndex(0);
+            setArticles(articles);
+        });
     }, []);
+
+    function previousPage() {
+        setSelectedIndex(selectedIndex - 1)
+    }
+
+    function nextPage() {
+        setSelectedIndex(selectedIndex + 1)
+    }
 
     return (
         <ArticlesViewContainer>
             <ArticlesViewHeader>
                 <ArticleTitle>Title</ArticleTitle>
             </ArticlesViewHeader>
-            <ArticlesViewBody dangerouslySetInnerHTML={{__html: articles ? articles[0].body : ''}}/>
+            <ArticlesViewBody dangerouslySetInnerHTML={{__html: articles ? articles[selectedIndex].body : ''}}/>
             <ArticlesViewFooter>
-
+                {articles ?
+                    <FooterContainer>
+                        <FooterPageIndex>
+                            {selectedIndex + 1} of {articles.length}
+                        </FooterPageIndex>
+                        <FooterNavigationButtons>
+                            {selectedIndex > 0 ?
+                                <NavigationButton onClick={previousPage}>Previous</NavigationButton> : ''}
+                            {selectedIndex < articles.length - 1 ?
+                                <NavigationButton onClick={nextPage}>Next</NavigationButton> : ''}
+                        </FooterNavigationButtons>
+                    </FooterContainer> : ''}
             </ArticlesViewFooter>
         </ArticlesViewContainer>
     );
--- frontend/src/components/articles-view/ArticlesViewStyles.js
+++ frontend/src/components/articles-view/ArticlesViewStyles.js
@@ -2,6 +2,7 @@ import styled, {css} from "styled-components";
 
 const defaultBar = css`
   height: 64px;
+  flex-shrink: 0;
   width: 100%;
   background-color: #1091bd;
   display: flex;
@@ -33,4 +34,41 @@ export const ArticlesViewBody = styled.div`
 export const ArticlesViewFooter = styled.footer`
   ${defaultBar};
   margin: auto auto 0;
+`;
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export const FooterPageIndex = styled.div`
+  margin: auto auto auto 15px;
+  font-size: 18px;
+  color: white;
+`;
+
+export const FooterNavigationButtons = styled.div`
+  margin: auto 0 auto auto;
+`;
+
+export const NavigationButton = styled.button`
+  margin: auto 15px auto 0;
+  background: none;
+  color: white;
+  font-size: 24px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: background-color 0.2s linear 0s;
+  padding: 5px 10px;
+  border-radius: 5px;
+  
+  &:hover {
+    background-color: rgba(255,255,255,0.2);
+  }
+  
+  &:active {
+    background-color: rgba(195,195,195,0.2);
+    transition: none;
+  }
 `;
