df1a4ce8b2b59466bf7beaeba397df0397e197fd
francescodist <francesco.dstf@gmail.com>
2019-10-01 12:43:27 +0100

    added global setup for articles

--- frontend/src/tests/articles/ArticlesActions.test.js
+++ frontend/src/tests/articles/ArticlesActions.test.js
@@ -1,13 +1,11 @@
 import {fetchArticles, searchArticles, selectArticleById} from "../../actions";
+import {articlesSetup} from "./ArticlesSetup";
 
 let articles;
 let mockFetchPromise;
 
 beforeEach(function () {
-    articles = [
-        {id: 1, title: 'Title 1', body: '<div id="test1">Body</div>', authors: ['auth1','auth2']},
-        {id: 2, title: 'Title 2', body: '<div id="test2">Body</div>', authors: ['auth1']},
-    ];
+    articles = articlesSetup();
     mockFetchPromise = Promise.resolve({
         json: () => Promise.resolve({data: articles}),
     });
---
+++ frontend/src/tests/articles/ArticlesSetup.js
@@ -0,0 +1,6 @@
+export function articlesSetup() {
+    return [
+        {id: 1, title: 'Title 1', body: '<div id="test1">Body</div>', authors: ['auth1', 'auth2']},
+        {id: 2, title: 'Title 2', body: '<div id="test2">Body</div>', authors: ['auth1']},
+    ]
+}
