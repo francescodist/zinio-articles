2d09728f6f61168e39b544e40a0557e7f84ae761
francescodist <francesco.dstf@gmail.com>
2019-10-01 10:11:08 +0100

    added tests for article actions

---
+++ frontend/src/tests/articles/ArticlesActions.test.js
@@ -0,0 +1,54 @@
+import {fetchArticles, searchArticles, selectArticleById} from "../../actions";
+
+let articles;
+let mockFetchPromise;
+
+beforeEach(function () {
+    articles = [
+        {id: 1, title: 'Title 1', body: '<div id="test1">Body</div>', authors: ['auth1','auth2']},
+        {id: 2, title: 'Title 2', body: '<div id="test2">Body</div>', authors: ['auth1']},
+    ];
+    mockFetchPromise = Promise.resolve({
+        json: () => Promise.resolve({data: articles}),
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+});
+
+afterEach(() => {
+    global.fetch.mockClear();
+});
+
+describe('fetchArticles', () => {
+    it('returns fetched articles', async () => {
+        const res = await fetchArticles();
+        expect(res).toStrictEqual(articles);
+    });
+
+    it('prefixes images src with "https://cdn2.audiencemedia.com"', async () => {
+        articles[0].body = '<img src="/test"/>';
+        const res = await fetchArticles();
+        expect(res[0].body).toBe('<img src="https://cdn2.audiencemedia.com/test"/>');
+    })
+});
+
+describe('searchArticles', () => {
+    it('returns only the articles searched', () => {
+        //search by title
+        let res = searchArticles(articles, 'Title 2');
+        expect(res.length).toBe(1);
+        expect(res[0]).toStrictEqual(articles[1]);
+        //search by author
+        res = searchArticles(articles, 'auth2');
+        expect(res[0]).toStrictEqual(articles[0]);
+        //search by body content
+        res = searchArticles(articles, 'Body');
+        expect(res).toStrictEqual(articles);
+    });
+});
+
+describe('selectArticleById', () => {
+    it('selects the right article by id', () => {
+        const res = selectArticleById(articles, 1);
+        expect(res).toStrictEqual(articles[0]);
+    })
+});
